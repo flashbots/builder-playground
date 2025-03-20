69ce5ad22a87fed38d12b1c1cfe72a8d750b8d50
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-03-20 22:21:11 +0100

    Explicit log

--- internal/artifacts.go
+++ internal/artifacts.go
@@ -421,6 +421,7 @@ func (o *output) LogOutput(name string) (*os.File, error) {
 }
 
 func (o *output) WriteFile(dst string, data interface{}) error {
+	fmt.Println("writing file", dst)
 	dst = filepath.Join(o.dst, dst)
 
 	var dataRaw []byte
