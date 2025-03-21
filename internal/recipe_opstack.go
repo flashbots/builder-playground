27c4f0c79348a120c9ed8a201c79c08df32bb0c8
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-03-21 11:42:23 +0100

    Add deterministic p2p key (#62)

--- internal/recipe_opstack.go
+++ internal/recipe_opstack.go
@@ -56,7 +56,9 @@ func (o *OpRecipe) Apply(ctx *ExContext, artifacts *Artifacts) *Manifest {
 		L1Beacon: "beacon",
 		L2Node:   elNode,
 	})
-	svcManager.AddService("op-geth", &OpGeth{})
+	svcManager.AddService("op-geth", &OpGeth{
+		UseDeterministicP2PKey: o.externalBuilder != "",
+	})
 	svcManager.AddService("op-batcher", &OpBatcher{
 		L1Node:     "el",
 		L2Node:     "op-geth",
