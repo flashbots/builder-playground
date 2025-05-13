e3274b1b8c2dd633276974b718c2daaad6f5829e
fredo <luehrs.fred@gmail.com>
2025-05-13 13:46:16 +0200

    fix: prefund the batcher's address on L1

--- playground/artifacts.go
+++ playground/artifacts.go
@@ -304,6 +304,12 @@ func (b *ArtifactsBuilder) Build() (*Artifacts, error) {
 			}
 		}
 
+		// Note(fredo): This is the predefined Batcher address from utils/intent.toml
+		allocs[gethcommon.HexToAddress("0xafF0CA253b97e54440965855cec0A8a2E2399896").String()] = map[string]interface{}{
+			"balance": fmt.Sprintf("0x%x", prefundedBalance),
+			"nonce":   "0x1",
+		}
+
 		newOpGenesis, err := overrideJSON(opGenesis, input)
 		if err != nil {
 			return nil, err
