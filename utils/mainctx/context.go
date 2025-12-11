03560d8f3a077f742357ddbb6f6045f7761b7b26
Caner Çıdam <canercidam01@gmail.com>
2025-12-12 02:20:13 +0300

    add a signal-aware main context

---
+++ utils/mainctx/context.go
@@ -0,0 +1,35 @@
+package mainctx
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"log"
+)
+
+var (
+	sigCh       = make(chan os.Signal, 1)
+	sigAwareCtx context.Context
+)
+
+func init() {
+	var cancel context.CancelFunc
+	sigAwareCtx, cancel = context.WithCancel(context.Background())
+	signal.Notify(sigCh,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		sig := <-sigCh
+		log.Printf("received signal: %s", sig)
+		cancel()
+	}()
+}
+
+// Get returns a context that is aware of the interruption/termination signals received by the process.
+func Get() context.Context {
+	return sigAwareCtx
+}
