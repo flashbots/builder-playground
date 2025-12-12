7830f9a6794023357f02c3b8f9ae40ff8e5f9ed3
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-12-12 12:04:58 +0100

    Do not return docker-compose output on ctx cancelled

--- playground/local_runner.go
+++ playground/local_runner.go
@@ -1095,6 +1095,10 @@ func (d *LocalRunner) Run(ctx context.Context) error {
 	cmd.Stderr = &errOut
 
 	if err := cmd.Run(); err != nil {
+		// Don't return error if context was cancelled
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
 		return fmt.Errorf("failed to run docker-compose: %w, err: %s", err, errOut.String())
 	}
 
