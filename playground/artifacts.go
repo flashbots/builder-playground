d03b0e0cb98f8b7b7a45df6558ecf9bf6478ee4b
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2025-12-18 05:45:33 -0500

    Adding possibility to add more prefunded accounts (#221)
    Add possibility to add more prefunded accounts to the L1 generated
    genesis by accepting a repeated `[]string` flag `--prefunded-account`
    which accepts the same format (private key in hex) as the static
    prefunded accounts.
    
    I debated about putting the `prefunded-account` flag on the L1 recipe
    directly, to scope it just to this. Maybe it would be preferable, let me
    know what do you think.

--- playground/artifacts.go
+++ playground/artifacts.go
@@ -69,6 +69,7 @@ type ArtifactsBuilder struct {
 	applyLatestL2Fork    *uint64
 	l1BlockTimeInSeconds uint64
 	opBlockTimeInSeconds uint64
+	prefundedAccounts    []string
 }
 
 func NewArtifactsBuilder() *ArtifactsBuilder {
@@ -109,6 +110,32 @@ func (b *ArtifactsBuilder) L1BlockTime(blockTimeSeconds uint64) *ArtifactsBuilde
 func (b *ArtifactsBuilder) OpBlockTime(blockTimeSeconds uint64) *ArtifactsBuilder {
 	b.opBlockTimeInSeconds = blockTimeSeconds
 	return b
+}
+
+func (b *ArtifactsBuilder) PrefundedAccounts(accounts []string) *ArtifactsBuilder {
+	b.prefundedAccounts = accounts
+	return b
+}
+
+var staticPrefundedAccounts = []string{
+	"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+	"0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+	"0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+	"0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+	"0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
+	"0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
+	"0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e",
+	"0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356",
+	"0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97",
+	"0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
+}
+
+func (b *ArtifactsBuilder) getPrefundedAccounts() []string {
+	if len(b.prefundedAccounts) > 0 {
+		return append(append([]string{}, staticPrefundedAccounts...), b.prefundedAccounts...)
+	}
+
+	return staticPrefundedAccounts
 }
 
 type Artifacts struct {
@@ -171,7 +198,7 @@ func (b *ArtifactsBuilder) Build() (*Artifacts, error) {
 	// add pre-funded accounts
 	prefundedBalance, _ := new(big.Int).SetString("10000000000000000000000", 16)
 
-	for _, privStr := range prefundedAccounts {
+	for _, privStr := range b.getPrefundedAccounts() {
 		priv, err := getPrivKey(privStr)
 		if err != nil {
 			return nil, err
@@ -294,7 +321,7 @@ func (b *ArtifactsBuilder) Build() (*Artifacts, error) {
 		// Update the allocs to include the same prefunded accounts as the L1 genesis.
 		allocs := make(map[string]interface{})
 		input["alloc"] = allocs
-		for _, privStr := range prefundedAccounts {
+		for _, privStr := range b.getPrefundedAccounts() {
 			priv, err := getPrivKey(privStr)
 			if err != nil {
 				return nil, err
@@ -687,19 +714,6 @@ func isByteArray(t reflect.Type) bool {
 
 func isByteSlice(t reflect.Type) bool {
 	return t.Kind() == reflect.Slice && t.Elem().Kind() == reflect.Uint8
-}
-
-var prefundedAccounts = []string{
-	"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-	"0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-	"0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-	"0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
-	"0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
-	"0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
-	"0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e",
-	"0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356",
-	"0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97",
-	"0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
 }
 
 func applyTemplate2(templateStr []byte, input interface{}) ([]byte, error) {
