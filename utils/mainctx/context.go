2822af9a50ed874e9316d4f0fc5db2a35ce03611
Caner Çıdam <canercidam01@gmail.com>
2026-02-06 17:37:48 +0100

    wrap signal handling in a new implementation and improve testing

--- utils/mainctx/context.go
+++ utils/mainctx/context.go
@@ -8,45 +8,64 @@ import (
 	"syscall"
 )
 
-var (
-	sigCh              = make(chan os.Signal, 1)
+type signalHandler struct {
+	sigCh              chan os.Signal
+	sigCount           int
 	gracefulCtx        context.Context
 	gracefulCtxCancel  context.CancelFunc
 	forceKillCtx       context.Context
 	forceKillCtxCancel context.CancelFunc
-)
+}
+
+func newSignalHandler(sigCh chan os.Signal) *signalHandler {
+	gracefulCtx, gracefulCtxCancel := context.WithCancel(context.Background())
+	forceKillCtx, forceKillCtxCancel := context.WithCancel(context.Background())
+	return &signalHandler{
+		sigCh:              sigCh,
+		sigCount:           0,
+		gracefulCtx:        gracefulCtx,
+		gracefulCtxCancel:  gracefulCtxCancel,
+		forceKillCtx:       forceKillCtx,
+		forceKillCtxCancel: forceKillCtxCancel,
+	}
+}
+
+func (sh *signalHandler) handle() {
+	for sig := range sh.sigCh {
+		sh.sigCount++
+		if sh.sigCount == 1 {
+			slog.Warn("received signal, shutting down gracefully... (interrupt 2 more times force kill)", "signal", sig)
+			sh.gracefulCtxCancel()
+		} else if sh.sigCount == 2 {
+			slog.Warn("received signal again (interrupt 1 more time to force kill)", "signal", sig)
+		} else if sh.sigCount >= 3 {
+			slog.Warn("force killing...")
+			sh.forceKillCtxCancel()
+		}
+	}
+}
+
+var sigHandler *signalHandler
 
 func init() {
-	gracefulCtx, gracefulCtxCancel = context.WithCancel(context.Background())
-	forceKillCtx, forceKillCtxCancel = context.WithCancel(context.Background())
+	sigCh := make(chan os.Signal, 1)
+	sigHandler = newSignalHandler(sigCh)
 	signal.Notify(sigCh,
 		syscall.SIGHUP,
 		syscall.SIGINT,
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
-	go func() {
-		sigCount := 0
-		for sig := range sigCh {
-			sigCount++
-			if sigCount == 1 {
-				slog.Warn("received signal, shutting down gracefully... (interrupt 2 more times force kill)", "signal", sig)
-				gracefulCtxCancel()
-			} else if sigCount == 2 {
-				slog.Warn("received signal again (interrupt 1 more time to force kill)", "signal", sig)
-			} else if sigCount >= 3 {
-				slog.Warn("force killing...")
-				forceKillCtxCancel()
-			}
-		}
-	}()
+	go sigHandler.handle()
 }
 
 // Get returns a context that is aware of the interruption/termination signals
 // received by the process. It is intended for graceful exits.
 func Get() context.Context {
-	return gracefulCtx
+	return sigHandler.gracefulCtx
 }
 
+// GetForceKillCtx returns the context that is aware of the interruption/termination signals
+// received by the process. It is intended for force kills.
 func GetForceKillCtx() context.Context {
-	return forceKillCtx
+	return sigHandler.forceKillCtx
 }
--- utils/mainctx/context_test.go
+++ utils/mainctx/context_test.go
@@ -2,114 +2,105 @@ package mainctx
 
 import (
 	"context"
+	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
+func TestSignalHandler_NewSignalHandler(t *testing.T) {
+	r := require.New(t)
+	sigCh := make(chan os.Signal, 1)
+	sh := newSignalHandler(sigCh)
+
+	r.NotNil(sh)
+	r.NotNil(sh.gracefulCtx)
+	r.NotNil(sh.forceKillCtx)
+	r.Equal(0, sh.sigCount)
+
+	// Contexts should not be cancelled initially
+	select {
+	case <-sh.gracefulCtx.Done():
+		r.Fail("graceful context should not be cancelled initially")
+	default:
+	}
+
+	select {
+	case <-sh.forceKillCtx.Done():
+		r.Fail("force kill context should not be cancelled initially")
+	default:
+	}
+}
+
+func TestSignalHandler_ContextsAreDifferent(t *testing.T) {
+	r := require.New(t)
+	// Send only one signal - graceful ctx should be cancelled, force kill should not
+	sigCh := make(chan os.Signal, 1)
+	sigCh <- testSignal{}
+	close(sigCh)
+
+	sh := newSignalHandler(sigCh)
+	sh.handle()
+
+	r.Equal(context.Canceled, sh.gracefulCtx.Err())
+	r.NoError(sh.forceKillCtx.Err())
+}
+
+func TestSignalHandler_FirstSignalCancelsGracefulContext(t *testing.T) {
+	r := require.New(t)
+	sigCh := make(chan os.Signal, 1)
+	sigCh <- testSignal{}
+	close(sigCh)
+
+	sh := newSignalHandler(sigCh)
+	sh.handle()
+
+	r.Equal(1, sh.sigCount)
+	r.Equal(context.Canceled, sh.gracefulCtx.Err())
+	r.NoError(sh.forceKillCtx.Err())
+}
+
+func TestSignalHandler_SecondSignalDoesNotCancelForceKill(t *testing.T) {
+	r := require.New(t)
+	sigCh := make(chan os.Signal, 2)
+	sigCh <- testSignal{}
+	sigCh <- testSignal{}
+	close(sigCh)
+
+	sh := newSignalHandler(sigCh)
+	sh.handle()
+
+	r.Equal(2, sh.sigCount)
+	r.Equal(context.Canceled, sh.gracefulCtx.Err())
+	r.NoError(sh.forceKillCtx.Err())
+}
+
+func TestSignalHandler_ThirdSignalCancelsForceKillContext(t *testing.T) {
+	r := require.New(t)
+	sigCh := make(chan os.Signal, 3)
+	sigCh <- testSignal{}
+	sigCh <- testSignal{}
+	sigCh <- testSignal{}
+	close(sigCh)
+
+	sh := newSignalHandler(sigCh)
+	sh.handle()
+
+	r.Equal(3, sh.sigCount)
+	r.Equal(context.Canceled, sh.gracefulCtx.Err())
+	r.Equal(context.Canceled, sh.forceKillCtx.Err())
+}
+
 func TestGet(t *testing.T) {
 	r := require.New(t)
 	ctx := Get()
-	r.NotNil(ctx, "Get() returned nil context")
-
-	// Verify the context is not already cancelled (before any signals)
-	select {
-	case <-ctx.Done():
-		r.Fail("graceful context should not be cancelled initially")
-	default:
-		// expected
-	}
+	r.NotNil(ctx)
 }
 
 func TestGetForceKillCtx(t *testing.T) {
 	r := require.New(t)
 	ctx := GetForceKillCtx()
-	r.NotNil(ctx, "GetForceKillCtx() returned nil context")
-
-	// Verify the context is not already cancelled (before any signals)
-	select {
-	case <-ctx.Done():
-		r.Fail("force kill context should not be cancelled initially")
-	default:
-		// expected
-	}
-}
-
-func TestContextsAreDifferent(t *testing.T) {
-	r := require.New(t)
-	graceful := Get()
-	forceKill := GetForceKillCtx()
-
-	r.NotEqual(graceful, forceKill, "graceful and force kill contexts should be different")
-}
-
-func TestContextsAreConsistent(t *testing.T) {
-	r := require.New(t)
-	// Multiple calls should return the same context
-	ctx1 := Get()
-	ctx2 := Get()
-	r.Equal(ctx1, ctx2, "Get() should return the same context on multiple calls")
-
-	fk1 := GetForceKillCtx()
-	fk2 := GetForceKillCtx()
-	r.Equal(fk1, fk2, "GetForceKillCtx() should return the same context on multiple calls")
-}
-
-func TestGracefulContextCancellation(t *testing.T) {
-	r := require.New(t)
-	// Send a signal to trigger graceful shutdown
-	sigCh <- testSignal{}
-
-	ctx := Get()
-	select {
-	case <-ctx.Done():
-		r.Equal(context.Canceled, ctx.Err())
-	case <-time.After(time.Second * 10):
-		r.Fail("graceful context should be cancelled after first signal")
-	}
-
-	// Force kill context should still be active after first signal
-	fkCtx := GetForceKillCtx()
-	select {
-	case <-fkCtx.Done():
-		r.Fail("force kill context should not be cancelled after first signal")
-	default:
-		// expected
-	}
-}
-
-func TestSecondSignalDoesNotCancelForceKill(t *testing.T) {
-	r := require.New(t)
-	// Send second signal
-	sigCh <- testSignal{}
-	sigCh <- testSignal{}
-
-	// Force kill context should still be active after second signal
-	fkCtx := GetForceKillCtx()
-	select {
-	case <-fkCtx.Done():
-		r.Fail("force kill context should not be cancelled after second signal")
-	default:
-		// expected
-	}
-}
-
-func TestForceKillContextCancellation(t *testing.T) {
-	r := require.New(t)
-	// Send third signal
-	sigCh <- testSignal{}
-	sigCh <- testSignal{}
-	sigCh <- testSignal{}
-
-	// Force kill context should be cancelled
-	fkCtx := GetForceKillCtx()
-	select {
-	case <-fkCtx.Done():
-		r.Equal(context.Canceled, fkCtx.Err())
-	case <-time.After(time.Second * 10):
-		r.Fail("force kill context should be cancelled after third signal")
-	}
+	r.NotNil(ctx)
 }
 
 // testSignal implements os.Signal for testing purposes
